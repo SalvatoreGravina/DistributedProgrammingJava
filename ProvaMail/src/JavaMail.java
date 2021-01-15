0cf42c53064dfe051b341b1df33b2dcf3d2025ea
FerdyGuar97 <52747015+FerdyGuar97@users.noreply.github.com>
2021-01-15 02:16:40 +0100

    JavaMail

---
+++ ProvaMail/src/JavaMail.java
@@ -0,0 +1,11 @@
+
+
+
+public class JavaMail {
+
+
+    public static void main(String[] args) throws Exception {
+        JavaMailUtils.sendMail("vincidis@gmail.com");
+    }
+    
+}
---
+++ ProvaMail/src/JavaMailUtils.java
@@ -0,0 +1,54 @@
+
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class JavaMailUtils {
+
+    public static void sendMail(String recepient) throws Exception{
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String myAccountEmail = "PizzeriaDiem@gmail.com";
+        String password = "PizzaDiem2020";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+
+        Message message = prepareMessage(session, myAccountEmail, recepient);
+        
+        Transport.send(message);
+        System.out.println("Message sent successfully");
+    }
+
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+            message.setSubject("Pizzeria DIEM - ordine n. 1");
+            String htmlCode = "<h1> Ecco il tuo ordine </h1> <br/> <h2><b>Ci devi molti soldi</b></h2>";
+            message.setContent(htmlCode, "text/html");
+            return message;
+        } catch (MessagingException ex) {
+        }
+        return null;
+    }
+
+}
