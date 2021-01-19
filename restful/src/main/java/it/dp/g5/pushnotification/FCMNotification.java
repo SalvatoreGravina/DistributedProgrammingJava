314d2214f2277f8acb1644958d02d3800035f3d9
FerdyGuar97 <52747015+FerdyGuar97@users.noreply.github.com>
2021-01-19 13:14:12 +0100

    update FCMNotification

--- restful/src/main/java/it/dp/g5/pushnotification/FCMNotification.java
+++ restful/src/main/java/it/dp/g5/pushnotification/FCMNotification.java
@@ -16,7 +16,7 @@ public class FCMNotification {
     public final static String AUTH_KEY_FCM = "AAAAhwlg8DM:APA91bG3nkIKSzPaz8NrXcYPMldnHj3IAIg_QwSd7ceqMUwSaTL2G4m2Zh8uCyAC7FS81yhNyO_JxYWMbHARHpFVWXmHrvCqOEbIUs8cuy7Zl1h_N_DqxoOuH4jvgkZ3fW6gy99Oj0oO";
     public final static String API_URL_FCM = "https://fcm.googleapis.com/fcm/send";
 
-    public static void pushFCMNotification(String DeviceIdKey) throws Exception {
+    public static void pushFCMNotification(String DeviceIdKey, String title, String text) throws Exception {
 
         String authKey = AUTH_KEY_FCM; // You FCM AUTH key
         String FMCurl = API_URL_FCM;
@@ -35,8 +35,8 @@ public class FCMNotification {
         JSONObject data = new JSONObject();
         data.put("to", DeviceIdKey.trim());
         JSONObject info = new JSONObject();
-        info.put("title", "FCM Notificatoin Title"); // Notification title
-        info.put("text", "Hello First Test notification"); // Notification body
+        info.put("title", title); // Notification title
+        info.put("text", text); // Notification body
         data.put("notification", info);
 
         OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
@@ -60,6 +60,6 @@ public class FCMNotification {
 
     @SuppressWarnings("static-access")
     public static void main(String[] args) throws Exception {
-        FCMNotification.pushFCMNotification("USER_DEVICE_TOKEN");
+        FCMNotification.pushFCMNotification("USER_DEVICE_TOKEN", "Pizza", "Prova");
     }
 }
