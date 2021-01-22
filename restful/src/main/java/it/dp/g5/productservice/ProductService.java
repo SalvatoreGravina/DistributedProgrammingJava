36363d1226e6a5eb92ab8048c262644168b6ee78
MrJoky13 <52746655+MrJoky13@users.noreply.github.com>
2021-01-22 20:37:28 +0100

    JavaDoc ProductService

--- restful/src/main/java/it/dp/g5/productservice/ProductService.java
+++ restful/src/main/java/it/dp/g5/productservice/ProductService.java
@@ -6,6 +6,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+* Classe che mappa le risorse relative ai prodotti come risorse web service.
+*
+* @author Davide Della Monica
+* @author Vincenzo di Somma
+* @author Salvatore Gravina
+* @author Ferdinando Guarino
+*/
+ 
+/**
+* path relativo ai servizi offerti per il prodotto
+*/
 
 @Path("/ProductService")
 
@@ -14,6 +26,13 @@ public class ProductService {
     ProductDAO productDao = new ProductDAO();
     private static final String SUCCESS_RESULT = "<result>success</result>";
     private static final String FAILURE_RESULT = "<result>failure</result>";
+    
+    /**
+     * Restituisce tutti i prodotti disponibili alla
+     * ricezione di una GET
+     *
+     * @return una stringa contente tutti gli prodotti
+     */
     
     @GET
     @Path("/products")
