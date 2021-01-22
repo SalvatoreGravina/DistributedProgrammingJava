10d95147bc0aabbdb58200725b25c8699c1df4bc
FerdyGuar97 <52747015+FerdyGuar97@users.noreply.github.com>
2021-01-23 00:39:59 +0100

    FCMNotification JavaDoc

--- restful/src/main/java/it/dp/g5/pushnotification/FCMNotification.java
+++ restful/src/main/java/it/dp/g5/pushnotification/FCMNotification.java
@@ -1,6 +1,5 @@
 package it.dp.g5.pushnotification;
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -8,18 +7,32 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.JSONObject;
 
-
-
+/**
+ * Classe che fornisce i metodi statici per mandare notifiche push ad un
+ * determinato device.
+ *
+ * @author Davide Della Monica
+ * @author Vincenzo di Somma
+ * @author Salvatore Gravina
+ * @author Ferdinando Guarino
+ */
 public class FCMNotification {
 
-    // Method to send Notifications from server to client end.
     public final static String AUTH_KEY_FCM = "AAAAhwlg8DM:APA91bG3nkIKSzPaz8NrXcYPMldnHj3IAIg_QwSd7ceqMUwSaTL2G4m2Zh8uCyAC7FS81yhNyO_JxYWMbHARHpFVWXmHrvCqOEbIUs8cuy7Zl1h_N_DqxoOuH4jvgkZ3fW6gy99Oj0oO";
     public final static String API_URL_FCM = "https://fcm.googleapis.com/fcm/send";
 
+    /**
+     * Manda una notifica push
+     *
+     * @param DeviceIdKey TokenID univoco relativo al dispositivo android di target della notifica
+     * @param title Titolo della notifica
+     * @param text Corpo della notifica
+     * @throws java.lang.Exception errore durante l'invio della notifica
+     */
     public static void pushFCMNotification(String DeviceIdKey, String title, String text) throws Exception {
 
-        String authKey = AUTH_KEY_FCM; // You FCM AUTH key
-        String FMCurl = API_URL_FCM;
+        String authKey = AUTH_KEY_FCM;      // Authentication key del server FireCloud
+        String FMCurl = API_URL_FCM;        // API per l'invio di notifiche
 
         URL url = new URL(FMCurl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -35,8 +48,8 @@ public class FCMNotification {
         JSONObject data = new JSONObject();
         data.put("to", DeviceIdKey.trim());
         JSONObject info = new JSONObject();
-        info.put("title", title); // Notification title
-        info.put("text", text); // Notification body
+        info.put("title", title); 
+        info.put("text", text);
         data.put("notification", info);
 
         OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
