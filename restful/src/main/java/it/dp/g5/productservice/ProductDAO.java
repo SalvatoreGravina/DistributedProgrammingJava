700bb40bcfffab267813331f37f6325d8d9c2c05
MrJoky13 <52746655+MrJoky13@users.noreply.github.com>
2021-01-22 21:10:33 +0100

    JavaDoc ProductDAO

--- restful/src/main/java/it/dp/g5/productservice/ProductDAO.java
+++ restful/src/main/java/it/dp/g5/productservice/ProductDAO.java
@@ -6,10 +6,22 @@ import it.dp.g5.backend.Database;
 import it.dp.g5.order.Product;
 import java.util.List;
 
-
+/**
+ * Classe che permette di definire i metodi CRUD per i prodotti.
+ *
+ * @author Davide Della Monica
+ * @author Vincenzo di Somma
+ * @author Salvatore Gravina
+ * @author Ferdinando Guarino
+ */
 public class ProductDAO {
     private Database db = Database.getInstance();
 
+    /**
+     * Restituisce tutti i prodotti disponibili.
+     *
+     * @return una stringa contente tutti gli ordini
+     */
     public String getAllProducts() {
         return db.getMenu();
     }
