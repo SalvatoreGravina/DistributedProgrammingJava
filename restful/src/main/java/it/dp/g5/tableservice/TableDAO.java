20185366c04222f6e9c338c8815fbbfe0f3dd747
MrJoky13 <52746655+MrJoky13@users.noreply.github.com>
2021-01-22 21:11:56 +0100

    JavaDoc TableDAO

--- restful/src/main/java/it/dp/g5/tableservice/TableDAO.java
+++ restful/src/main/java/it/dp/g5/tableservice/TableDAO.java
@@ -9,12 +9,21 @@ import it.dp.g5.backend.Database;
 import java.util.Map;
 
 /**
+ * Classe che permette di definire i metodi CRUD per i tavoli.
  *
- * @author gruppo 5
+ * @author Davide Della Monica
+ * @author Vincenzo di Somma
+ * @author Salvatore Gravina
+ * @author Ferdinando Guarino
  */
 public class TableDAO {
     private Database db = Database.getInstance();
     
+    /**
+     * Restituisce tutti i tavoli disponibili.
+     *
+     * @return una stringa contente tutti i tavoli disponibili
+     */
     public String getFreeTables(){
         return db.getFreeTablesDB();
     }
